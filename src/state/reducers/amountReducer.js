02952c25e1ac63849dd1c409e698e888fc17eb48
JoyCode4 <joywadhonkar4@gmail.com>
2023-07-28 11:37:04 +0530

    created the reducers index and action-creator index and also added the index and store in the state dir and create the store

--- src/state/reducers/amountReducer.js
+++ src/state/reducers/amountReducer.js
@@ -1,11 +1,13 @@
-export default reducer=(state=0,action)=>{
-    if(action.type=='deposite'){
+const reducers=(state=0,action)=>{
+    if(action.type==='deposite'){
         return state+action.payload;
     }
-    else if(action.type=='withdraw'){
+    else if(action.type==='withdraw'){
         return state-action.payload;
     }
     else{
         return state;
     }
 }
+
+export default reducers;
