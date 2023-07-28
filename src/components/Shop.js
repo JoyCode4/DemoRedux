e630ac1327449fb85c3b1dd850f47830814426a4
JoyCode4 <joywadhonkar4@gmail.com>
2023-07-28 10:59:04 +0530

    navbar and shop components are created

---
+++ src/components/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <>
+    <h1>Buy Adidas Shoes 50 Rs</h1>
+        <div>
+            <button className='btn btn-primary'>-</button>
+            <span className='mx-4'>Add to cart</span>
+            <button className='btn btn-primary'>+</button>
+        </div>
+    </>
+  )
+}
+
+export default Shop
