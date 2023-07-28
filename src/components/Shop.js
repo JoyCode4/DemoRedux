d437304e92cdb6073d00582fedb9abdcee1cf5c5
JoyCode4 <joywadhonkar4@gmail.com>
2023-07-28 11:19:28 +0530

    created the state redux action and reducer and also install the redux, react-redux and redux-thunk npm package

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Shop = () => {
   return (
     <>
-    <h1>Buy Adidas Shoes 50 Rs</h1>
+    <h1>Deposite/Withdraw Money</h1>
         <div>
             <button className='btn btn-primary'>-</button>
-            <span className='mx-4'>Add to cart</span>
+            <span className='mx-4'>Update Balance</span>
             <button className='btn btn-primary'>+</button>
         </div>
     </>
